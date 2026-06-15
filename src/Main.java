92ab5959f4968b0db1247dcb1348528322669c6c
Sudeep Sharma <sudeepofficial22@gmail.com>
2026-06-15 23:20:44 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Pawn pawn = new Pawn(Color.WHITE);
+        Color pawncolor = pawn.getColor();
+        System.out.println(pawncolor);
+    }
+}
---
+++ src/Position.java
@@ -0,0 +1,22 @@
+public class Position {
+    private final int row;
+    private final int col;
+    private static final int MAX_INDEX = 7;
+    private static final int MIN_INDEX = 0;
+    public Position(int row,int col){
+        if(row>=MIN_INDEX &&  row<=MAX_INDEX && col>=MIN_INDEX && col<=MAX_INDEX){
+            this.row = row;
+            this.col = col;
+        }
+        else{
+            throw new IllegalArgumentException("Invalid Position values");        }
+    }
+
+    public int getRow(){
+        return row;
+    }
+    public int getCol(){
+        return col;
+    }
+
+}
